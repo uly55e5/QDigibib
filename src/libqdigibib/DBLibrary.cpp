8d30573ab0009be7413065c7a0640770ff49d46f
uly55e5 <uly55e5@web.de>
2009-08-08 02:01:59 +0200

    Implemented methods for path management in DBLibrary

--- src/libqdigibib/DBLibrary.cpp
+++ src/libqdigibib/DBLibrary.cpp
@@ -2,10 +2,12 @@
  * DBLibrary.cpp
  *
  *  Created on: 06.08.2009
- *      Author: dave
+ *      Author: uly55e5
  */
 
 #include "DBLibrary.h"
+
+#include <QDir>
 
 namespace QDigibib
 {
@@ -19,6 +21,36 @@ DBLibrary::DBLibrary()
 DBLibrary::~DBLibrary()
 {
 	// TODO Auto-generated destructor stub
+}
+
+void DBLibrary::addPath(const QString & path)
+{
+	if (!_libraryPaths.contains(path))
+		_libraryPaths.append(path);
+}
+
+QStringList DBLibrary::paths()
+{
+	return _libraryPaths;
+}
+
+void DBLibrary::setPaths(const QStringList & paths)
+{
+	_libraryPaths = paths;
+}
+
+bool DBLibrary::removePath(const QString & path)
+{
+	if (_libraryPaths.contains(path))
+		return _libraryPaths.removeOne(path);
+	else
+	{
+		QDir dir(path);
+		QString cPath = dir.canonicalPath();
+		if (_libraryPaths.contains(cPath))
+			return _libraryPaths.removeOne(cPath);
+	}
+	return false;
 }
 
 }
--- src/libqdigibib/DBLibrary.h
+++ src/libqdigibib/DBLibrary.h
@@ -2,27 +2,72 @@
  * DBLibrary.h
  *
  *  Created on: 06.08.2009
- *      Author: dave
+ *      Author: uly55e5
  */
 
 #ifndef DBLIBRARY_H_
 #define DBLIBRARY_H_
 
 #include <QObject>
+#include <QStringList>
 
 namespace QDigibib
 {
-/*! This class manages a set of volumes from the "Digitale Bibliothek"
+/*! \brief Manages a set of volumes from the "Digitale Bibliothek"
+ *
+ * A library is defined by a set of paths to folders, which are searched
+ * recursively for the volumes. Use setPaths(), addPath() and removePath() to
+ * manage these folders. Get the folders with paths().
  *
  */
-class DBLibrary:public QObject
+class DBLibrary: public QObject
 {
-	Q_OBJECT
+Q_OBJECT
 public:
 	DBLibrary();
 	virtual ~DBLibrary();
+
+	/*! \brief List of paths used for at library creation.
+	 *
+	 * \sa addPath(), removePath(), setPaths()
+	 *
+	 * \return A list of absolute paths
+	 */
+	QStringList paths();
+
+	/*! \brief Set the paths for the current library.
+	 *
+	 * The paths should be absolute canonical paths of folders.
+	 *
+	 * \sa paths(), addPath()
+	 *
+	 * \param paths A list of absolute paths
+	 */
+	void setPaths(const QStringList & paths);
+
+	/*! \brief Add a path to the library paths.
+	 *
+	 * The path should be a canonical path of a folder.
+	 *
+	 * \sa removePath(), setPaths()
+	 * \param path An absolute path
+	 */
+	void addPath(const QString & path);
+
+	/*! \brief Remove a path from the library paths.
+	 *
+	 *  First the method tries to match the path against the list of paths
+	 *  by string comparison. If this fails the canonical path is used.
+	 *
+	 *  \sa addPath(), paths()
+	 *
+	 * \param path An absolute path
+	 * \return True on success
+	 */
+	bool removePath(const QString & path);
+
 private:
-	QStringList _libraryPaths; ///< the paths where to search for volumes
+	QStringList _libraryPaths; ///< The paths where to search for volumes
 };
 
 }
