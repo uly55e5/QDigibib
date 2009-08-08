e6c41538d308b90ae579b4968c3a638273dfd671
uly55e5 <uly55e5@web.de>
2009-08-08 02:01:06 +0200

    small updates in documentation of DBLibraryHelper

--- src/libqdigibib/DBLibraryHelper.h
+++ src/libqdigibib/DBLibraryHelper.h
@@ -13,27 +13,27 @@
 namespace QDigibib
 {
 
-/*! Helper methods for Library management
+/*! \brief Helper methods for Library management
  *
- * This class contains (mostly static) methodes for which help at management of
+ * This class contains (mostly static) methods for which help at management of
  * the DBLibrary.
  */
 class DBLibraryHelper: public QObject
 {
 Q_OBJECT
-	/*! Find files in a directory tree.
+public:
+	/*! \brief Find files in a directory tree.
 
 	 The method searches for all files with name fileName in a directory tree
 	 starting at the rootDir. The search is not case sensitive. A list of
 	 files with absolute paths is returned. If one of the arguments is an
 	 empty string, an empty list is returned.
 
-	 \param fileName file name to search for
-	 \param startPath  path where to start searching
-	 \param recursive if TRUE search in subdirectories
-	 \return list of files with absolute paths
+	 \param fileName File name to search for
+	 \param startPath  Path where to start searching
+	 \param recursive If TRUE search in subdirectories
+	 \return A List of files with absolute paths
 	 */
-public:
 	static QStringList
 	findFiles(const QString & fileName, const QString & startPath,
 			bool recursive = true);
