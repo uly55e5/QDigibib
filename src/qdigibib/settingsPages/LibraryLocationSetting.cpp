2bda0c3efb7c7d1de62648a08e44fa929b94ad70
uly55e5 <uly55e5@web.de>
2009-08-11 00:56:02 +0200

    GUI: New selection for library paths

--- src/qdigibib/settingsPages/LibraryLocationSetting.cpp
+++ src/qdigibib/settingsPages/LibraryLocationSetting.cpp
@@ -6,6 +6,11 @@ LibraryLocationSetting::LibraryLocationSetting(QWidget *parent) :
 	SettingsPageInterface(parent)
 {
 	ui.setupUi(this);
+	FileSystemModel * model = new FileSystemModel(this);
+	model->setRootPath(QDir::rootPath());
+	model->setFilter(QDir::Dirs | QDir::NoDotAndDotDot | QDir::Drives);
+	ui.treeView->setModel(model);
+	ui.treeView->setRootIndex(model->index(QDir::rootPath()));
 }
 
 LibraryLocationSetting::~LibraryLocationSetting()
@@ -21,4 +26,97 @@ QString LibraryLocationSetting::name()
 {
 	return tr("Library");
 }
+
+FileSystemModel::FileSystemModel(QObject * parent) :
+	QFileSystemModel(parent)
+{
+
+}
+
+FileSystemModel::~FileSystemModel()
+{
+
+}
+
+Qt::ItemFlags FileSystemModel::flags(const QModelIndex & index) const
+{
+	// items are checkable and tristate
+	return (Qt::ItemIsSelectable | Qt::ItemIsUserCheckable | Qt::ItemIsTristate
+			| Qt::ItemIsEnabled);
+}
+
+QVariant FileSystemModel::data(const QModelIndex & index, int role) const
+{
+	if (role == Qt::CheckStateRole)
+	{
+		// return unchecked for root index
+		if (index == QModelIndex())
+			return Qt::Unchecked;
+		// return saved states if available
+		if (_checkStates.contains(index))
+			return _checkStates[index];
+		// return same as parent if not saved
+		else if (data(parent(index), role) != Qt::PartiallyChecked)
+			return data(parent(index), role);
+		// in other cases return unchecked
+		return Qt::Unchecked;
+	}
+	// for other roles return parent method
+	return QFileSystemModel::data(index, role);
+}
+
+bool FileSystemModel::setData(const QModelIndex & index,
+		const QVariant & value, int role)
+{
+	// return if nothing changed
+	if (index == QModelIndex() || value == data(index, role))
+		return true;
+	// accept only changes in CheckState
+	if (role == Qt::CheckStateRole)
+	{
+		// save state value
+		Qt::CheckState state = (Qt::CheckState) value.toInt();
+		// save checked items in Map
+		if (state != Qt::Unchecked)
+		{
+			_checkStates[index] = (Qt::CheckState) value.toInt();
+			setData(parent(index), Qt::PartiallyChecked, role);
+		}
+		// remove unchecked items from map
+		else if (_checkStates.contains(index) )
+		{
+			_checkStates.remove(index);
+			// test for childrens and parents checkstate
+			if(hasCheckedChildren(index))
+				_checkStates[index]=Qt::PartiallyChecked;
+			if(!hasCheckedChildren(parent(index)))
+				setData(parent(index),Qt::Unchecked,role);
+		}
+		// update model
+		emit layoutChanged();
+		return true;
+	}
+	// return false if role was not checkstate
+	return false;
+}
+
+bool FileSystemModel::hasCheckedChildren( const QModelIndex & ind ) const
+{
+	// check all Children of the index
+	for (int r=0; r <= rowCount(ind)-1; r++)
+	{
+		// return on first (partially) checked item found
+		if(data(index(r,0,ind),Qt::CheckStateRole) != Qt::Unchecked)
+			return true;
+	}
+	// return false is all unchecked
+	return false;
+}
+
+int FileSystemModel::columnCount(const QModelIndex & parent) const
+{
+	// use only the first column (name)
+	return 1;
+}
+
 }
--- src/qdigibib/settingsPages/LibraryLocationSetting.h
+++ src/qdigibib/settingsPages/LibraryLocationSetting.h
@@ -4,25 +4,76 @@
 #include "SettingsPageInterface.h"
 #include "ui_LibraryLocationSetting.h"
 
+#include <QFileSystemModel>
+#include <QMap>
+
 namespace QDigibib
 {
-/*! \brief This page allows to select the paths for the libraries.
- *
+/**
+ * \brief This page allows to select the paths for the libraries.
  */
 class LibraryLocationSetting: public SettingsPageInterface
 {
 Q_OBJECT
 
 public:
+	/**
+	 * \brief Constructs a new page of library location settings.
+	 *
+	 * \param parent The parent widget
+	 */
 	LibraryLocationSetting(QWidget *parent = 0);
+
+	/**
+	 * \brief Destroys the page.
+	 */
 	~LibraryLocationSetting();
 
-	// inheritetd from SettingsPageInterface
+	// inherited from SettingsPageInterface
 	virtual QIcon icon();
 	virtual QString name();
 
 private:
 	Ui::LibraryLocationSettingClass ui; ///< The UI class
 };
+
+/**
+ * \brief A file system model with checkable items.
+ */
+class FileSystemModel: public QFileSystemModel
+{
+Q_OBJECT
+public:
+	/**
+	 * \brief Constructs a new model
+	 *
+	 * @param parent The parent object
+	 */
+	FileSystemModel(QObject * parent = 0);
+
+	/**
+	 * \brief Destroys the model.
+	 */
+	virtual ~FileSystemModel();
+
+	// inherited from QFileSystemModel
+	Qt::ItemFlags flags(const QModelIndex & index) const;
+	QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+	bool setData(const QModelIndex & index, const QVariant & value, int role =
+			Qt::EditRole);
+	int columnCount(const QModelIndex & parent = QModelIndex()) const;
+
+private:
+	/**
+	 *  \brief Checks if any of the children are (partially) checked
+	 *
+	 * @param index The index of the parent item
+	 * @return True if at least one item is checked
+	 */
+	bool hasCheckedChildren(const QModelIndex & index) const;
+
+	QMap<QModelIndex, Qt::CheckState> _checkStates; ///< saved check states
+};
 }
+
 #endif // LIBRARYLOCATIONSETTING_H
